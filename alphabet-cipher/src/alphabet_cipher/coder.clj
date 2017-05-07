ce89a44f40d9fa56213f4f0dea5aae9934d2e2cd
Travis <travis.daudelin@gmail.com>
2017-05-06 21:44:30 -0500

    alphabet cipher solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,57 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(defn char-to-int
+  "Converts a char to a 0-indexed int. E.g. \\a == 0, \\b == 1 and so
+  on"
+  [char]
+  (- (int char) 97))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn int-to-char
+  "Converts an int modulus 26 into the corresponding char"
+  [int]
+  (char (+ (mod int 26) 97)))
 
-(defn decipher [cipher message]
-  "decypherme")
+(defn encode-char
+  "encodes a single char using the alphabet cipher and a provided key"
+  [char key]
+  (let [char-int (char-to-int char)
+        key-int (char-to-int key)]
+    (int-to-char (+ char-int key-int))))
 
+(defn decode-char
+  "decodes a single char using the alphabet cipher and a provided key"
+  [char key]
+  (let [char-int (char-to-int char)
+        key-int (char-to-int key)]
+    (int-to-char (- char-int key-int))))
+
+(defn alpha-cipher
+  [cipher-fn]
+  (fn [keyword message]
+    (let [message-len (count message)
+          keyword (take message-len (cycle keyword))
+          message (seq message)]
+      (->> (map cipher-fn message keyword)
+           (apply str)))))
+
+(defn encode
+  [keyword message]
+  (let [cipher (alpha-cipher encode-char)]
+    (cipher keyword message)))
+
+(defn decode
+  [keyword message]
+  (let [cipher (alpha-cipher decode-char)]
+    (cipher keyword message)))
+
+(defn decipher
+  [encoded-message message]
+  (let [cipher (alpha-cipher decode-char)
+        keyword-cycle (seq (cipher message encoded-message))
+        cycle-reducer (fn [keyword next-char]
+                        (let [cycle-length (count keyword-cycle)]
+                          (if (= (take cycle-length (cycle keyword))
+                                 keyword-cycle)
+                            (reduced keyword)
+                            (conj keyword next-char))))]
+    (apply str (reduce cycle-reducer [(first keyword-cycle)] (rest keyword-cycle)))))
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,48 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(defn equality-test
+  "Builds a simple equality test from the provided test function"
+  [test-fn]
+  (fn [expected-val test-fn-args]
+    (is (= expected-val (apply test-fn test-fn-args)))))
+
+(deftest test-char-to-int
+  (let [test-equal (equality-test char-to-int)]
+    (testing "converts charaters to correct integer"
+      (test-equal 0 [\a])
+      (test-equal 1 [\b])
+      (test-equal 2 [\c])
+      (test-equal 25 [\z]))))
+
+(deftest test-int-to-char
+  (let [test-equal (equality-test int-to-char)]
+    (testing "converts integers to correct characters"
+      (test-equal \a [0])
+      (test-equal \b [1])
+      (test-equal \c [2])
+      (test-equal \z [25])
+      ;; ints > 25 should wrap back to \a
+      (test-equal \f [31])
+      (test-equal \o [40])
+      (test-equal \w [100]))))
+
+(deftest test-encode-char
+  (let [test-equal (equality-test encode-char)]
+    (testing "correctly encodes chars given a key"
+      (test-equal \a [\a \a])
+      (test-equal \b [\a \b])
+      (test-equal \s [\g \m])
+      (test-equal \o [\z \p]))))
+
+(deftest test-decode-char
+  (let [test-equal (equality-test decode-char)]
+    (testing "correctly decodes chars given a key"
+      (test-equal \a [\a \a])
+      (test-equal \a [\b \b])
+      (test-equal \s [\l \t])
+      (test-equal \j [\p \g]))))
+
 (deftest test-encode
   (testing "can encode given a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
